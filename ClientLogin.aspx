fdf4d6fb2d9aa7a4f56e6770571f5338d8cac9cb
EMILYVTHOMAS1 <43903392+EMILYVTHOMAS1@users.noreply.github.com>
2019-03-26 16:35:47 +0530

    Login page for Client.

---
+++ ClientLogin.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/guest.master" AutoEventWireup="true" CodeFile="ClientLogin.aspx.cs" Inherits="Default3" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:Panel ID="Panel1" runat="server" 
+        
+        style="z-index: 1; left: 203px; top: 137px; position: absolute; height: 178px; width: 625px">
+        <asp:Label ID="Label1" runat="server" Text="CLIENT LOGIN" 
+            
+            style="z-index: 1; left: 303px; top: 30px; position: absolute; width: 120px; height: 20px"></asp:Label>
+        <asp:Label ID="Label2" runat="server" 
+            style="z-index: 1; left: 91px; top: 60px; position: absolute; height: 19px" 
+            Text="CLIENT ID"></asp:Label>
+        <asp:Label ID="Label3" runat="server" 
+            style="z-index: 1; left: 92px; top: 90px; position: absolute; height: 15px; width: 56px" 
+            Text="PASSWORD"></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server" 
+            style="z-index: 1; left: 303px; top: 67px; position: absolute; height: 19px"></asp:TextBox>
+        <asp:TextBox ID="TextBox2" runat="server" 
+            
+            style="z-index: 1; left: 301px; top: 94px; position: absolute; height: 22px; width: 121px; margin-top: 0px" 
+            TextMode="Password"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" onclick="Button1_Click1" 
+            style="z-index: 1; left: 481px; top: 86px; position: absolute; height: 22px" 
+            Text="SIGN IN" />
+    </asp:Panel>
+</asp:Content>
+
---
+++ ClientLogin.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class Default3 : System.Web.UI.Page
+{
+    connection a1;
+    SqlDataReader dr;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        a1 = new connection();
+    }
+    protected void Button1_Click1(object sender, EventArgs e)
+    {
+         string s;
+        s = "select * from login where userID='" + TextBox1.Text + "' and Password='" + TextBox2.Text + "'";
+        dr = a1.disp(s);
+        if (dr.HasRows == false)
+            Response.Write("<script>alert('Invalid');</script>");
+        else
+        {
+
+            string admappstatus = "", sappstatus = "";
+
+            s = "select * from clientapproval where clientid='" + TextBox1.Text + "'";
+            dr = a1.disp(s);
+            if (dr.Read())
+            {
+                sappstatus = dr.GetValue(1).ToString();
+                admappstatus = dr.GetValue(5).ToString();
+            }
+            if (sappstatus == "")
+                Response.Write("<script>alert('Your request not considered...Please wait');</script>");
+            else if (sappstatus == "N")
+                Response.Write("<script>alert('You request rejected ')script>");
+            else if (admappstatus == "")
+                Response.Write("<script>alert('Your request under process...Please wait');</script>");
+            else if (admappstatus == "N")
+                Response.Write("<script>alert('Your request rejected');</script>");
+            else
+            {
+                Session["clientid"] = TextBox1.Text;
+
+                Response.Redirect("clientprocess.aspx");
+            }
+        }
+
+    }
+}
