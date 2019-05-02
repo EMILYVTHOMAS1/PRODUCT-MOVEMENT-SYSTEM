831713efaaa7f272a97b6c88113764d08605ef53
EMILYVTHOMAS1 <43903392+EMILYVTHOMAS1@users.noreply.github.com>
2019-05-02 20:16:16 +0530

    Added rejected company details view

---
+++ showrejectedcompanydetails.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+
+public partial class Default3 : System.Web.UI.Page
+{
+    connection a1;
+    SqlDataReader dr;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        a1 = new connection();
+        String s;
+        s = "select * from companyreg where cmpid in(select cmpid from companyapproval where admappstatus='N' or sappstatus='N')";
+        SqlDataSource1.SelectCommand = s;
+        SqlDataSource1.DataBind();
+    }
+}
